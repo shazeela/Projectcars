823a5150cf3ceae4e52015fe238e27dd78608c5a
shazeela <shazkhairudin@gmail.com>
2021-05-25 21:27:44 -0400

    updated nissan model homework

--- src/shanaz/homework/App.java
+++ src/shanaz/homework/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, Welcome to the Car Kingdom!");
 
-        Nissan sentra = new Nissan("Sentra");
+        Nissan rogue = new Nissan("Sentra");
         BMW m3 = new BMW("M3");
         Ford mustang = new Ford("Mustang");
         Lincoln avaitor = new Lincoln("Avaitor");
@@ -13,7 +13,7 @@ public class App {
         Honda pilot = new Honda("pilot");
         Toyota corolla = new Toyota("Corolla");
 
-        System.out.printf("The Nissan model is %s \n", sentra.thisCar.getModel());
+        System.out.printf("The Nissan model is %s \n", rogue.thisCar.getModel());
         System.out.printf("The BMW model is %s \n", m3.thisCar.getModel());
         System.out.printf("The Ford model is %s \n", mustang.thisCar.getModel());
         System.out.printf("The Lincoln model is %s \n", avaitor.thisCar.getModel());
